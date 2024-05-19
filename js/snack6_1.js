21be23e6fafd2718e99cf876ff87d747597cee53
angelofaoldi <faoldiangelo@gmail.com>
2024-05-19 17:54:45 +0200

    6.1 [ADD] light bike variable

--- js/snack6_1.js
+++ js/snack6_1.js
@@ -1,15 +1,5 @@
 'use strict';
 
-const biciDaCorsa = [
-    {nome: "Bianchi Oltre XR4", peso: 7.2},
-    {nome: "Trek Madone SLR", peso: 6.8},
-    {nome: "Cannondale SuperSix Evo", peso: 7.0},
-    {nome: "Specialized Tarmac SL7", peso: 6.9},
-    {nome: "Pinarello Dogma F12", peso: 6.7}
-];
-
-console.log(biciDaCorsa);
-
 // scorrere l'elemento delle bici
 // cerchiamo di capire la bici più leggera
 // usiamo come riferimento il primo valore oppure avere un valore iniziale più grande dell'elenco
@@ -29,16 +19,39 @@ console.log(biciDaCorsa);
 // for (let nome of biciDaCorsa) {console.log(nome)}
 
 // creare una variabile LET per una bici leggera
-let biciLeggera = biciDaCorsa[4];
+// let biciLeggera = biciDaCorsa[4];
 
 // CICLO FOR
-for (let i = 0; 1 < biciDaCorsa.length; i++) {
-    if 
-}
+// for (let i = 0; 1 < biciDaCorsa.length; i++) {
+//     if 
+// }
 
 // o creare un array con solo le bici più leggere
 // o utilizzare direttamente filter che crea l'array ecc...
 
-biciLeggere.push
+// biciLeggere.push
 // e alla fine stamparci biciLeggere
+
+let biciDaCorsa = [
+    {nome: "Bianchi Oltre XR4", peso: 7.2},
+    {nome: "Trek Madone SLR", peso: 6.8},
+    {nome: "Cannondale SuperSix Evo", peso: 7.0},
+    {nome: "Specialized Tarmac SL7", peso: 6.9},
+    {nome: "Pinarello Dogma F12", peso: 6.7}
+];
+
+console.log(biciDaCorsa);
+
+/* 
+come fare per stamoare a schermo la bici più leggera?
+- iniziamo assumendo che la prima bici nell'array sia la più leggera
+- iteriamo successivamente per tutto l'array e
+    se troviamo una bici con un peso inferiore = la impostiamo come la nuova bici leggera
+- stampiamo il risultato del ciclo, cioò la bici più leggera
+*/
+
+// PRIMA BICI = BICI LEGGERA
+let biciLeggera = biciDaCorsa[0];
+
+
 
